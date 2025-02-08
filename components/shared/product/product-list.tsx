c93d2c2705348fbe93118d8d00deb609a58b0afa
nithinjose-tech <ninjos8@gmail.com>
2025-02-08 17:58:51 +0530

    first commit

---
+++ components/shared/product/product-list.tsx
@@ -0,0 +1,24 @@
+import ProductCard from './product-card';
+import { Product } from '@/types';
+
+
+const ProductList = ({ data, title }: { data: Product[]; title?: string }) => {
+    return (
+       <div className='my-10'>
+        <h2 className='h2-bold mb-4'>{title}</h2>
+        {data.length > 0 ? (
+            <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
+              {data.map((product: Product) => (
+               <ProductCard key={product.slug} product={product} />
+              ))}
+            </div>
+        ) : (
+          <div>
+            <p>No product found</p>
+          </div>
+        )}
+      </div>
+    );
+  };
+  
+  export default ProductList;
