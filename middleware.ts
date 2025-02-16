ad5f3aad009217eddc1832e889426f254fd6e360
nithinjose-tech <ninjos8@gmail.com>
2025-02-16 21:56:37 +0530

    change added

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,1 @@
-// export { auth as middleware } from '@/auth';
-
-import { GetServerSidePropsContext } from 'next';
-
-export async function middleware(req: GetServerSidePropsContext) {
-    const { auth } = await import('@/auth');
-    return auth(req);
-  }
-  
+export { auth as middleware } from '@/auth';
